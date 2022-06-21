efb86168d7bd4564825c5267a5f9ebc9dcfa7855
Sieva-cmd <musyokasieva@gmail.com>
2022-06-21 14:09:44 +0300

    addded navigation bar and styled

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import {Routes, Route } from 'react-router-dom';
 import AllMeetup from './pages/AllMeetup';
 import NewMeetup from './pages/NewMeetup';
 import Favourites from './pages/Favourites';
+import MainNavigation from './components/Layout/MainNavigation';
 
 
 
 const  App =() => {
   return (
     <>
+    <MainNavigation />
     <Routes>
      
-      <Route exact path='/' element = { <AllMeetup />}  ></Route> 
-      <Route path='/new-meetup' element = {   <NewMeetup />}  >
+      <Route exact path='/' element = {<AllMeetup />}  ></Route> 
+      <Route path='/new-meetup' element = { <NewMeetup />}  >
      
       </Route>
-      <Route path='/favourites' element ={ <Favourites />} >
+      <Route path='/favourites' element ={<Favourites />} >
        
       </Route> 
   
